b0a0c4029421ed3202ba6fc7895c788c77a16ab7
Matjias <117152858+denrigtigeMatjias@users.noreply.github.com>
2024-12-22 19:50:33 +0100

    Create main_loader.lua

---
+++ main_loader.lua
@@ -0,0 +1,13 @@
+if not import then
+    return
+end
+
+import('/important/config.lua')
+
+import('/autofarm/auto_cast.lua')
+import('/autofarm/auto_reel.lua')
+import('/autofarm/auto_shake.lua')
+import('/autofarm/freeze.lua')
+
+import('/ui/dashboard.lua')
+import('/ui/load_gui.lua')
