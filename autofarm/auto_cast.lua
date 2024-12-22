e606a9f243dd027d2185b7f9c5d45d99bb254843
Matjias <117152858+denrigtigeMatjias@users.noreply.github.com>
2024-12-22 20:43:02 +0100

    Update auto_cast.lua

--- autofarm/auto_cast.lua
+++ autofarm/auto_cast.lua
@@ -1,27 +1,38 @@
-spawn(function()
-    while wait(0.1) do
-        if getgenv().casttoggle then
-            local player = game.Players.LocalPlayer
-            local rodSearchTerm = "Rod"
-            local character = game:GetService("Workspace"):FindFirstChild(player.Name)
-            local equippedRod
+getgenv().auto_cast = function()
+    local player = game:GetService("Players").LocalPlayer
+    local character = player.Character
+    local rodSearchTerm = "Rod"
+    local equippedRod
 
-            if character then
-                for _, item in ipairs(character:GetChildren()) do
-                    if item.Name == "RodBodyModel" then
-                        continue
-                    elseif string.find(item.Name:lower(), rodSearchTerm:lower()) then
-                        equippedRod = item
-                        break
-                    end
-                end
-            end
-
-            if equippedRod and equippedRod:FindFirstChild("events") and equippedRod.events:FindFirstChild("cast") then
-                equippedRod.events.cast:FireServer(100, 1) -- Adjust parameters as needed
-            else
-                warn("No equipped rod or rod events found.")
+    if character then
+        for _, item in ipairs(character:GetChildren()) do
+            if item:IsA("Tool") and (item.Name:find("rod") or item.Name:find("Rod")) then
+                equippedRod = item
             end
         end
     end
-end)
+
+    if equippedRod and equippedRod:FindFirstChild("events") and equippedRod.events:FindFirstChild("cast") then
+        equippedRod.events.cast:FireServer(100, 1)
+    else
+        local rod = getgenv().findItemInLocalBackpack(rodSearchTerm)
+
+        if rod then
+            local equipEvent = getgenv().config.PlayerGui:FindFirstChild("hud") and getgenv().config.PlayerGui.hud.safezone.backpack.events:FindFirstChild("equip")
+
+            if equipEvent then
+                equipEvent:FireServer(rod)
+
+                wait(0.5)
+
+                if getgenv().config.character then
+                    for _, item in ipairs(getgenv().config.character:GetChildren()) do
+                        if item:IsA("Tool") and (item.Name:find("rod") or item.Name:find("Rod")) then
+                            equippedRod = item
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
